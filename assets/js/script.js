21f03eb7cbb5aa96ee63b74ee34ac1dd8bab0f52
jhonnatthan <jhonnatthans@outlook.com>
2019-03-09 17:13:55 -0300

    [ADD] - Estrutura

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+(function() {
+    
+})();
+
+function montaEstrutura() {
+    
+}
+
+function inserirAntesDe(posicao, todo) {
+
+}
+
+function inserirDepoisDe(posicao, todo) {
+
+}
+
+function inserirFim(todo) {
+}
+
+function inserirInicio(todo) {
+
+}
+
+function removerTodo(posicao) {
+
+}
+
+function alterarTodo(posicao, todo) {
+
+}
+
+function buscaTodo(todo) {
+    
+}
